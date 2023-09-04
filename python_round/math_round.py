90dbfcec5c0328e98b511f39eb03336892e7e866
arisia <starlights2283@gmail.com>
2023-09-04 22:29:45 +0900

    add python-round

---
+++ python_round/math_round.py
@@ -0,0 +1,14 @@
+import math
+
+value = -5.76543
+x = math.ceil(value)
+print(x)
+# 내림!
+
+x = math.floor(value)
+print(x)
+# 올림!
+
+x = math.trunc(value)
+print(x)
+# 버림!
---
+++ python_round/normal_round.py
@@ -0,0 +1,7 @@
+value = 5.682221
+print(type(value))
+
+x = round((value), 1)
+print(x)
+
+# 내장함수
